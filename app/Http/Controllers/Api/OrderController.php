386201460a3fe3a7b6dbab6ace0b704e3ebd71c6
Marzio Perez <marzioperez@gmail.com>
2018-04-28 18:57:03 -0500

    Generación de token de pedido PayMe

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Client;
 use App\Models\Inventory;
 use App\Models\InventoryMovement;
 use App\Models\Order;
@@ -20,8 +21,71 @@ class OrderController extends Controller
      * @return \Illuminate\Http\JsonResponse
      * @throws \Exception
      */
-    public function store(Request $request)
-    {
+
+    public function generateOrder(Request $request){
+        $order = $request->input('order');
+        $store_branche_id = $request->input('store_branche_id');
+
+        $branche = StoreBranch::find($store_branche_id);
+
+        $store = Store::find($branche->store_id);
+        $client = Client::find($order["client_id"]);
+
+        $occode = Order::select(['id'])->orderBy('id', 'desc')->first();
+        if($occode){
+            $order['order_code'] = '0' . ($occode->id + 1) . '-' . date("Y");
+        }else{
+            $order['order_code'] = '0' . 1 . '-' . date("Y");
+        }
+
+        //Clave SHA-2 de Wallet
+        $claveSecretaWallet = 'nXBqMLdasxQQurMz.422979735';
+
+        $registerVerification = openssl_digest(
+            $store->payme_comerce_id .
+            $client->id .
+            $client->email .
+            $claveSecretaWallet,
+            'sha512');
+
+        //Referencia al Servicio Web de Wallet
+        $wsdl = 'https://integracion.alignetsac.com/WALLETWS/services/WalletCommerce?wsdl';
+        // Producción
+        //$wsdl = 'https://www.pay-me.pe/WALLETWS/services/WalletCommerce?wsdl';
+
+        $client_soap = new \SoapClient($wsdl);
+
+        $params = array(
+            'idEntCommerce' => $store->payme_comerce_id,
+            'codCardHolderCommerce'=> $client->id,
+            'names' => $client->first_name,
+            'lastNames' => $client->last_name,
+            'mail' => $client->email,
+            'registerVerification' => $registerVerification
+        );
+
+        //Consumo del metodo RegisterCardHolder
+        $result = $client_soap->RegisterCardHolder($params);
+
+        //Se definen todos los parametros obligatorios.
+        $acquirerId = $store->payme_acquirer_id;
+        $idCommerce = $store->payme_comerce_id;
+        $purchaseOperationNumber = $order["order_code"];
+        $purchaseAmount = $order["total"];
+        $purchaseCurrencyCode = '604';
+
+        $claveSecretaPasarela = 'LbABXJkbcaFRLJchXCb?679658268743';
+
+        $purchaseVerification = openssl_digest($acquirerId . $idCommerce . $purchaseOperationNumber . $purchaseAmount . $purchaseCurrencyCode . $claveSecretaPasarela, 'sha512');
+
+        $data = array(
+            "purchaseVerification" => $purchaseVerification,
+            "result" => $result
+        );
+        return response()->json($data);
+    }
+
+    public function store(Request $request){
         $response='';
         $data='';
         $order = $request->input('order');
@@ -30,14 +94,16 @@ class OrderController extends Controller
         $store_branche_id = $request->input('store_branche_id');
         $delivery = $request->input('delivery');
 
-
         $store = Store::find($order['store_id']);
         $branch = $store->branches()->where('id', $store_branche_id)->first();
+
         //Codigo de orden
-
         $occode = Order::select(['id'])->orderBy('id', 'desc')->first();
-
-        $order['order_code'] = '0' . ($occode->id + 1) . '-' . date("Y");
+        if($occode){
+            $order['order_code'] = '0' . ($occode->id + 1) . '-' . date("Y");
+        }else{
+            $order['order_code'] = '0' . 1 . '-' . date("Y");
+        }
 
         $destination_store_branch = [
             "contact_person" => $store->comercial_contact->name,
@@ -50,65 +116,63 @@ class OrderController extends Controller
 
         ];
 
+        //return response()->json(['status' => 'ok', 'data' => $destination_store_branch]);
+
         DB::beginTransaction();
-
         try {
-
-
             //Cabecera
             $data = Order::create($order);
             //Detalle
             foreach ($orderdetails as $orderdetail) {
-
                 $orderdetail['order_id'] = $data->id;
                 $od = OrderDetail::create($orderdetail);
-
             }
-            //Inventario
 
+            //Inventario
             $inventory = Inventory::where('product_id', $od['product_id'])->where('store_branche_id', $store_branche_id)->first();
 
             if(isset($inventory)){
-                        $inventory->update([
-                            'quantity' => $inventory->quantity - $od['quantity']
-                        ]);
+                $inventory->update([
+                    'quantity' => $inventory->quantity - $od['quantity']
+                ]);
 
-                        InventoryMovement::create([
-                            'inventory_id' => $inventory->id,
-                            'quantity' => $od['quantity'],
-                            'order_id'=>$data->id,
-                            'movement_type' => 'E'
-                        ]);
-
+                InventoryMovement::create([
+                    'inventory_id' => $inventory->id,
+                    'quantity' => $od['quantity'],
+                    'order_id'=>$data->id,
+                    'movement_type' => 'E'
+                ]);
             }
+
             //Order in Urbaner
             if($delivery){
-            $json = [
-                "type" => "1",
-                "destinations" => [
-                    $destination_store_branch,
-                    $destination_client,
-                ],
-                "payment" => [
-                    "backend" => "card",
-                    "args" => [
-                        "bankcard" => 261
-                    ]
-                ],
-                "description" => "comida",
-                "vehicle_id" => "2",
-                "memo" => $data->order_code,
-                //"programmed_date" => "2017-11-10 13:00:00",
-                "is_return" => false,
-                "has_extended_search_time" => "true",
-               // "coupon" => "MY_REGISTERED_COUPON"
-            ];
-            $response = UrbanerUtil::apipost($json, UrbanerUtil::API_CLI_ORDER);
+                $json = [
+                    "type" => "1",
+                    "destinations" => [
+                        $destination_store_branch,
+                        $destination_client,
+                    ],
+                    "payment" => [
+                        "backend" => "card",
+                        "args" => [
+                            "bankcard" => 261
+                        ]
+                    ],
+                    "description" => "comida",
+                    "vehicle_id" => "2",
+                    "memo" => $data->order_code,
+                    "is_return" => false,
+                    "has_extended_search_time" => "true",
+                ];
+                $response = UrbanerUtil::apipost($json, UrbanerUtil::API_CLI_ORDER);
+                $od->update([
+                    "tracking_url" => $response->tracking,
+                    "tracking_code" => $response->code,
+                ]);
             }
             
         } catch (\Exception $e) {
             DB::rollback();
-            //throw new \Exception($e->getMessage());
             return response()->json([
                 'status' => 'error',
                 'order' => $data,
@@ -128,12 +192,7 @@ class OrderController extends Controller
 
 
     public function calculateDelivery(Request $request){
-
         $data = $request->all();
-        $store_branche_id = $data['store_branche_id'];
-
-        $storebranch = StoreBranch::find($store_branche_id);
-
         $storebranch = StoreBranch::find($data['store_branche_id']);
         $destinations = [
             'destinations' => [
@@ -143,62 +202,7 @@ class OrderController extends Controller
             "package_type_id"=> 1,
             "is_return"=> false
         ];
-
         $response = UrbanerUtil::apipost($destinations, UrbanerUtil::API_CLI_PRICE);
-
-        return $response;
-    }
-
-    public function paymentGenerate(Request $request){
-        //$store_id = Store::find()
-        $data = $request->all();
-
-        $store = Store::find($data["store_id"]);
-
-        $idEntCommerce = '420';
-        $codCardHolderCommerce = 'pruebawallet1';
-        $names = 'Arturo';
-        $lastNames = 'García';
-        $mail = 'arturo.garcia@regalalo.pe';
-
-        //Clave SHA-2 de Wallet
-        $claveSecretaWallet = 'nXBqMLdasxQQurMz.422979735';
-
-        $registerVerification = openssl_digest($idEntCommerce . $codCardHolderCommerce . $mail . $claveSecretaWallet, 'sha512');
-        //Referencia al Servicio Web de Wallet
-        $wsdl = 'https://integracion.alignetsac.com/WALLETWS/services/WalletCommerce?wsdl';
-
-        $client = new \SoapClient($wsdl);
-
-        $params = array(
-            'idEntCommerce'=>$idEntCommerce,
-            'codCardHolderCommerce'=>$codCardHolderCommerce,
-            'names'=>$names,
-            'lastNames'=>$lastNames,
-            'mail'=>$mail,
-            'registerVerification'=>$registerVerification
-        );
-
-        //Consumo del metodo RegisterCardHolder
-        $result = $client->RegisterCardHolder($params);
-
-        //Se definen todos los parametros obligatorios.
-        $acquirerId = '144'; //29
-        $idCommerce = '9092';
-        $purchaseOperationNumber = '10542';
-        $purchaseAmount = $data["total"];
-        $purchaseCurrencyCode = '604';
-
-        $claveSecretaPasarela = 'LbABXJkbcaFRLJchXCb?679658268743';
-
-        $purchaseVerification = openssl_digest($acquirerId . $idCommerce . $purchaseOperationNumber . $purchaseAmount . $purchaseCurrencyCode . $claveSecretaPasarela, 'sha512');
-
-        $data = array(
-            "purchaseVerification" => $purchaseVerification,
-            "result" => $result
-        );
-        return response()->json($data);
-
         return response()->json($response);
     }
 }
